feec2bd0474d4ffbec67d84b9ffa66d22ef6c33e
Jakob Kuen <jakkuen@gmail.com>
2015-10-13 20:01:55 +0200

    Neuer Index
    Index liest aus datenbank

---
+++ main.js
@@ -0,0 +1,49 @@
+var selectedDevice = 0;
+var selectedWeek = 0;
+
+$(document).ready(function() {
+	updateTable();
+	$("#week1").children().first().html(getWeek(0));
+	$("#week2").children().first().html(getWeek(1));
+	$("#week3").children().first().html(getWeek(2));
+
+	$(".cBtn").each(function(i, e){
+		$(e).click(function(){
+			updateTable();
+			updateClasses(i, ".cBtn");
+			selectedDevice = i;
+		});
+	});
+
+	$(".cWeek").each(function(i, e){
+		$(e).click(function(){
+			updateTable();
+			updateClasses(i, ".cWeek");
+			selectedWeek = i;
+		});
+	});
+});
+
+function updateClasses(idx, str){
+	$(str).each(function(i, e){
+		$(e).removeClass("active");
+		if(i == idx)$(e).addClass("active");
+	});
+}
+
+function getWeek(i){
+	var str= "";
+	str += Date.parse("last monday").add({days: i*7}).toString("dd. MMM");
+	str += " - " + Date.parse("last monday").add({days: i*7+4}).toString("dd. MMM")
+	return str;
+}
+
+function updateTable(){
+	$.ajax({
+		url: "makeTable.php",
+		data: {week: selectedWeek, device: selectedDevice},
+		success: function(result){
+			$("#tableContainer").html(result);
+		}
+	});
+}
---
+++ makeTable.php
@@ -0,0 +1,50 @@
+<?php
+	include("mysql.php");
+	$wk = array(
+		0 => "8:10 - 9:00",
+		1 => "9:00 - 9:55",
+		2 => "10:05 - 10:55",
+		3 => "11:05 - 11:55",
+		4 => "12:05 - 12:55",
+		5 => "13:00 - 13:50",
+		6 => "13:50 - 14:40",
+		7 => "14:40 - 15:30",
+		8 => "15:30 - 16:20",
+		9 => "16:20 - 17:10",
+		10 => "17:10 - 18:00",
+		11 => "18:00 - 18:50"
+		);
+	echo "<table class='table table-striped'>";
+	echo "<tr>
+				<th>Uhrzeit</th>
+        		<th>Montag</th>
+        		<th>Dienstag</th>
+        		<th>Mittwoch</th>
+        		<th>Donnerstag</th>
+        		<th>Freitag</th>
+        	</tr>";
+    $week = $_GET["week"];
+    $device = $_GET["device"];
+    $date = new DateTimeImmutable("last monday +$week week");
+    for($i = 0; $i < 12; $i++){
+		$stunde = $i+1;
+    	echo "<th>".$wk[$i]."</th>";
+    	$q = mysqli_query($conn, "SELECT * FROM res WHERE Stunde = $stunde AND Tag BETWEEN '".$date->format("Y-m-d")."' AND '".$date->modify("+4 days")->format("Y-m-d")."';");
+    	if(mysqli_fetch_array($q) == null){
+    		for($d = 0; $d < 5; $d++){
+    			echo "<td>Frei</td>";
+    		}
+    	}else{
+			for($d = 0; $d < 5; $d++){
+    			$q = mysqli_query($conn, "SELECT * FROM res WHERE Tag = '".$date->modify('+'.$d.' days')->format("Y-m-d")."' AND Stunde = $stunde");
+    			if(mysqli_fetch_array($q) == null){
+    				echo "<td>Frei</td>";
+    			}else{
+    				echo "<td>Reserviert</td>";
+    			}
+			}
+    	}
+    	echo "<tr>";
+    }
+    echo "</table>";
+?>
