bf7b8e31ce19fd8ef1d9328289e6302be8da2b08
Alphazork <alexandermeixner@alex-mbp.home>
2015-10-17 17:12:02 +0200

    Anzeige der reservierten Geräte

--- insert.php
+++ insert.php
@@ -4,10 +4,12 @@ $Lehrer=$_GET['lehrer'];
 $when=explode("_", $_GET['time']);
 $woche=$_GET['woche'];
 $device=$_GET['device'];
-echo $Lehrer;
-echo $woche;
-echo $when[1];
+echo "Eingeloggt als:".$Lehrer;
 $date = new DateTimeImmutable("last monday +$woche week");
 $q = mysqli_query($conn, "INSERT INTO res (Date,Stunde,Lehrer,DeviceID) values ('".$date->modify('+'.$when[1].' days')->format("Y-m-d")."','".$when[0]."',0,'".$device."');");
-
+echo ", reserviert:";
+$q2 = mysqli_query($conn, "SELECT * FROM res WHERE Lehrer ='".$Lehrer."'");
+while ($row = mysqli_fetch_assoc($q2)) {
+	echo " ".$row['DeviceID'];
+}
 ?>
